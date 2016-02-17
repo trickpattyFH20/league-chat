6f7618c24798017d0d0076c274338e2e4625b92c
trickpattyFH20 <patricktlawler@gmail.com>
2016-02-17 05:31:40 -0500

    server

--- app.js
+++ app.js
@@ -79,8 +79,9 @@ var server = http.createServer(app);
  */
 
 //server.listen(port);
+console.log(process.env)
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = '54.200.145.197' || '127.0.0.1'
 server.timeout = 0;
 server.listen(server_port, server_ip_address, function(){
     console.log("Listening on " + server_ip_address + ", server_port " + server_port)
